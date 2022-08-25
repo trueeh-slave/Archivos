8f1e2e09598090512d03f8e2b9e5c70a2c8a4dff
trueeh-slave <steamtruesnowace@gmail.com>
2022-08-25 16:55:13 -0500

    Final

--- src/co/edu/unbosque/controller/FileManager.java
+++ src/co/edu/unbosque/controller/FileManager.java
@@ -16,178 +16,212 @@ public class FileManager {
                          2. Intersección entre 1 y 2\s
                          3. Archivo 1 - Archivo 2\s
                          4. Archivo 2 - Archivo 1\s
-                         5. Salir"""));
+                         5. Borrar resultado.txt\s
+                         6. Salir
+                         \s
+                         Nota: si desea realizar una nueva opción, debe eliminar el archivo"""));
 
                 switch (menu) {
                     case 1 -> {
                         merge();
                         JOptionPane.showMessageDialog(null, "Archivo resultado.txt creado exitosamente");
-                        salir = true;
                     }
                     case 2 -> {
                         intersection();
                         JOptionPane.showMessageDialog(null, "Archivo resultado.txt creado exitosamente");
-                        salir = true;
                     }
                     case 3 -> {
                         differenceBetween1And2();
                         JOptionPane.showMessageDialog(null, "Archivo resultado.txt creado exitosamente");
-                        salir = true;
                     }
                     case 4 -> {
                         differenceBetween2And1();
                         JOptionPane.showMessageDialog(null, "Archivo resultado.txt creado exitosamente");
-                        salir = true;
                     }
-                    case 5 ->{
+                    case 5 -> deleteTxt();
+                    case 6 ->{
                         JOptionPane.showMessageDialog(null, "Que tenga una feliz tarde :D");
                         salir = true;
                     }
                 }
             }while (!salir);
-        } catch (Exception e){
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
-    public static void merge() throws IOException {
-        PrintWriter pw = new PrintWriter("archivo.txt");
-        BufferedReader br = new BufferedReader(new FileReader("E:\\Projects\\Archivos\\archivo1.txt"));
+    public static void merge() {
 
-        String line = br.readLine();
+        try{
+            PrintWriter pw = new PrintWriter("resultado.txt");
+            BufferedReader br = new BufferedReader(new FileReader("archivo1.txt"));
 
-        while (line != null) {
-            pw.println(line);
+            String line = br.readLine();
+
+            while (line != null) {
+                pw.println(line);
+                line = br.readLine();
+            }
+
+            br = new BufferedReader(new FileReader("archivo2.txt"));
+
             line = br.readLine();
+
+            while(line != null) {
+                pw.println(line);
+                line = br.readLine();
+            }
+
+            pw.flush();
+            br.close();
+            pw.close();
+
+        } catch (IOException e){
+            JOptionPane.showMessageDialog(null,"Error en el método merge");
         }
-
-        br = new BufferedReader(new FileReader("E:\\Projects\\Archivos\\archivo2.txt"));
-
-        line = br.readLine();
-
-        while(line != null) {
-            pw.println(line);
-            line = br.readLine();
-        }
-
-        pw.flush();
-        br.close();
-        pw.close();
 
     }
 
-    public static void intersection() throws IOException {
-        ArrayList<String> archivo1 = new ArrayList<>();
-        ArrayList<String> archivo2 = new ArrayList<>();
-        ArrayList<String> archivo3 = new ArrayList<>();
+    public static void intersection() {
 
-        FileWriter writer = new FileWriter("archivo.txt");
-        BufferedReader br = new BufferedReader(new FileReader("E:\\Projects\\Archivos\\archivo1.txt"));
+        try{
 
-        String line = br.readLine();
+            ArrayList<String> archivo1 = new ArrayList<>();
+            ArrayList<String> archivo2 = new ArrayList<>();
+            ArrayList<String> archivo3 = new ArrayList<>();
 
-        while(line != null){
-            line = br.readLine();
-            archivo1.add(line);
-        }
+            FileWriter writer = new FileWriter("resultado.txt");
+            BufferedReader br = new BufferedReader(new FileReader("archivo1.txt"));
 
+            String line = br.readLine();
 
-        br = new BufferedReader(new FileReader("E:\\Projects\\Archivos\\archivo2.txt"));
-        line = br.readLine();
-
-        while(line != null) {
-            line = br.readLine();
-            archivo2.add(line);
-        }
-
-        for(String linea : archivo1){
-            if(archivo2.contains(linea)){
-                archivo3.add(linea);
+            while(line != null){
+                line = br.readLine();
+                archivo1.add(line);
             }
-        }
 
-        for(String cadaLinea : archivo3){
-            writer.write(cadaLinea + System.lineSeparator());
-            System.out.println(cadaLinea);
+
+            br = new BufferedReader(new FileReader("archivo2.txt"));
+            line = br.readLine();
+
+            while(line != null) {
+                line = br.readLine();
+                archivo2.add(line);
+            }
+
+            for(String linea : archivo1){
+                if(archivo2.contains(linea)){
+                    archivo3.add(linea);
+                }
+            }
+
+            for(String cadaLinea : archivo3){
+                writer.write(cadaLinea + System.lineSeparator());
+                System.out.println(cadaLinea);
+            }
+            writer.close();
+            br.close();
+
+        } catch (IOException e){
+            JOptionPane.showMessageDialog(null,"Error en el método intersection");
         }
-        writer.close();
-        br.close();
     }
 
-    public static void differenceBetween1And2() throws IOException {
-        ArrayList<String> archivo1 = new ArrayList<>();
-        ArrayList<String> archivo2 = new ArrayList<>();
-        ArrayList<String> archivo3 = new ArrayList<>();
+    public static void differenceBetween1And2() {
 
-        FileWriter writer = new FileWriter("archivo.txt");
-        BufferedReader br = new BufferedReader(new FileReader("E:\\Projects\\Archivos\\archivo1.txt"));
+        try{
+            ArrayList<String> archivo1 = new ArrayList<>();
+            ArrayList<String> archivo2 = new ArrayList<>();
+            ArrayList<String> archivo3 = new ArrayList<>();
 
-        String line = br.readLine();
+            FileWriter writer = new FileWriter("resultado.txt");
+            BufferedReader br = new BufferedReader(new FileReader("archivo1.txt"));
 
-        while(line != null){
-            line = br.readLine();
-            archivo1.add(line);
-        }
+            String line = br.readLine();
 
-        br = new BufferedReader(new FileReader("E:\\Projects\\Archivos\\archivo2.txt"));
-        line = br.readLine();
-
-        while(line != null) {
-            line = br.readLine();
-            archivo2.add(line);
-        }
-
-        for(String linea : archivo1){
-            if(!archivo2.contains(linea)){
-                archivo3.add(linea);
+            while(line != null){
+                line = br.readLine();
+                archivo1.add(line);
             }
-        }
 
-        for(String cadaLinea : archivo3){
-            writer.write(cadaLinea + System.lineSeparator());
-            System.out.println(cadaLinea);
+            br = new BufferedReader(new FileReader("archivo2.txt"));
+            line = br.readLine();
+
+            while(line != null) {
+                line = br.readLine();
+                archivo2.add(line);
+            }
+
+            for(String linea : archivo1){
+                if(!archivo2.contains(linea)){
+                    archivo3.add(linea);
+                }
+            }
+
+            for(String cadaLinea : archivo3){
+                writer.write(cadaLinea + System.lineSeparator());
+                System.out.println(cadaLinea);
+            }
+            writer.close();
+            br.close();
+
+        } catch (IOException e){
+            JOptionPane.showMessageDialog(null,"Error en el método differenceBetween1And2");
         }
-        writer.close();
-        br.close();
     }
 
     public static void differenceBetween2And1() throws IOException {
-        ArrayList<String> archivo1 = new ArrayList<>();
-        ArrayList<String> archivo2 = new ArrayList<>();
-        ArrayList<String> archivo3 = new ArrayList<>();
 
-        FileWriter writer = new FileWriter("archivo.txt");
-        BufferedReader br = new BufferedReader(new FileReader("E:\\Projects\\Archivos\\archivo1.txt"));
+        try{
 
-        String line = br.readLine();
+            ArrayList<String> archivo1 = new ArrayList<>();
+            ArrayList<String> archivo2 = new ArrayList<>();
+            ArrayList<String> archivo3 = new ArrayList<>();
 
-        while(line != null){
-            line = br.readLine();
-            archivo1.add(line);
-        }
+            FileWriter writer = new FileWriter("resultado.txt");
+            BufferedReader br = new BufferedReader(new FileReader("archivo1.txt"));
 
-        br = new BufferedReader(new FileReader("E:\\Projects\\Archivos\\archivo2.txt"));
+            String line = br.readLine();
 
-        line = br.readLine();
-
-        while(line != null) {
-            line = br.readLine();
-            archivo2.add(line);
-        }
-
-        for(String linea : archivo2){
-            if(!archivo1.contains(linea)){
-                archivo3.add(linea);
+            while(line != null){
+                line = br.readLine();
+                archivo1.add(line);
             }
-        }
 
-        for(String cadaLinea : archivo3){
-            writer.write(cadaLinea + System.lineSeparator());
-            System.out.println(cadaLinea);
-        }
-        writer.close();
+            br = new BufferedReader(new FileReader("archivo2.txt"));
 
-        System.out.println("termino todo");
+            line = br.readLine();
+
+            while(line != null) {
+                line = br.readLine();
+                archivo2.add(line);
+            }
+
+            for(String linea : archivo2){
+                if(!archivo1.contains(linea)){
+                    archivo3.add(linea);
+                }
+            }
+
+            for(String cadaLinea : archivo3){
+                writer.write(cadaLinea + System.lineSeparator());
+                System.out.println(cadaLinea);
+            }
+            writer.close();
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Error en el método differenceBetween2And1");
+        }
     }
 
+    public static void deleteTxt(){
+        try{
+            File file = new File("resultado.txt");
+            if(file.exists()){
+                file.delete();
+            }
 
+            JOptionPane.showMessageDialog(null,"Archivo eliminado correctamente");
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Ha ocurrido un error, compruebe si el archivo existe e intente nuevamente");
+        }
+    }
 }
